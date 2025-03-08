479860a788f474dc09da582e7b669749b2b6aadf
LorenzoCane <lorenzo.cane985@edu.unito.it>
2025-03-08 08:52:17 +0100

    20 ep

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ conf: Dict[str, Any] = {
     
     # Training hyperparameters
     "batch_size": 256,                       # Batch size for training and validation
-    "num_steps": 235*50,                      # Total number of training steps | CelebA original: (636 * 200), c_MNIST orig: (235*100)
+    "num_steps": 235*20,                      # Total number of training steps | CelebA original: (636 * 200), c_MNIST orig: (235*100)
     "num_workers": 1,                        # Number of worker processes for data loading
     
     # Optimizer settings
