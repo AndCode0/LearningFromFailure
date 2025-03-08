1a9334c8118417645763e618aa6f4ea769a33b17
LorenzoCane <lorenzo.cane985@edu.unito.it>
2025-03-08 08:52:08 +0100

    20 ep

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ conf: Dict[str, Any] = {
     "bias_attr_idx": 1,                     # 20 Male attribute index for CelebA, 
 
     "skew_ratio": 0.02,
-    "severity": 4,
+    "severity": 2,
     # Model configuration
     "model_tag": "SimpleConvNet",                 # Model architecture to use
     
